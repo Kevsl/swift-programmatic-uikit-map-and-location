30ec2deb8281be9d6c738c7d83846e3e721ab57a
Kevsl <ktr2@pm.me>
2023-10-03 12:32:05 +0200

    feat: added displays of searched cities in Apple map

--- mapTest/Controller/ViewController.swift
+++ mapTest/Controller/ViewController.swift
@@ -60,61 +60,46 @@ class ViewController: UIViewController {
    
     }
     
-    @objc func searchCity(){
-       
-        guard let city = searchInput.text else {
+    @objc func searchCity() {
+        guard let searchValue = searchInput.text else {
             return
         }
-        
-        fetchDataFromAPI(city){
-            apiResponse in
+
+        fetchDataFromAPI(searchValue) { apiResponse in
             DispatchQueue.main.async {
                 if let apiResponse = apiResponse {
-                    
                     self.mapView.removeAnnotations(self.mapView.annotations)
-                    
+
                     for city in apiResponse.features {
-                        guard let latitude = city.geometry.coordinates.last, let longitude = city.geometry.coordinates.first else{
-                            return
-                        }
-                        
-                        self.addAnnotation(city.properties.name, city.properties.context, latitude, longitude)
+                        self.addAnnotation(city.properties.city, city.geometry.coordinates[1], city.geometry.coordinates[0], city.properties.context)
                     }
                     
-                
+                } else {
+                    print("Error fetching data from API")
                 }
+                self.zoomToAnnotations()
             }
-            
-            
-                
-         
         }
     }
 }
 
-
-extension ViewController: MKMapViewDelegate{
+extension ViewController:MKMapViewDelegate{
     func setupMap(){
-        
-        mapView.delegate = self
+             mapView = MKMapView()
+             mapView.delegate = self
         view.addSubview(mapView)
         
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
-            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150),
-            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
-        
-        
-        addAnnotation("Strasbourg", "info", 48.5870262, 7.7530259)
-        
-        addAnnotation("Paris", "Panthéon", 48.866667, 2.333333)
-        
+             mapView.translatesAutoresizingMaskIntoConstraints = false
+             NSLayoutConstraint.activate([
+                 mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+                 mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150),
+                 mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                 mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+             ])
+
+        addAnnotation("Aix les bains", 45.692341, 5.908998, "une ville ")
+        addAnnotation("Schiltigheim", 48.599998 , 7.75, "une autre ville")
     }
-    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
             let identifier = "pin"
             var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
@@ -130,22 +115,30 @@ extension ViewController: MKMapViewDelegate{
 
             return annotationView
         }
+
+        func addAnnotation(_ title: String, _ latitude: CLLocationDegrees, _ longitude: CLLocationDegrees, _ info: String) {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            annotation.title = title
+            annotation.subtitle = info
+            mapView.addAnnotation(annotation)
+        }
     
     
-    func addAnnotation(_ title:String, _ subtitle:String,_ latitude: CLLocationDegrees,_ longitude:CLLocationDegrees){
+    func zoomToAnnotations() {
         
-        
-        let annotation = MKPointAnnotation()
-        
-        annotation.title = title
-        annotation.subtitle = subtitle
-        annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        
-        mapView.addAnnotation(annotation)
-        
+        let annotations = self.mapView.annotations
+        if  annotations.count > 0 {
+            let region = MKCoordinateRegion(center: annotations.last!.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+        }
+        else{
+            print("nil")
+        }
     }
     
-}
+    }
+
 extension ViewController: CLLocationManagerDelegate {
     func setupCL(){
         locationManager.delegate = self
@@ -160,17 +153,17 @@ extension ViewController: CLLocationManagerDelegate {
             return
         }
         
-        addAnnotation("Apple", "siège", location.coordinate.latitude, location.coordinate.longitude)
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
             mapView.showsUserLocation = true
-            if let coordinate = locationManager.location?.coordinate {
-                let regionRadius: CLLocationDistance = 15000
-                let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-                mapView.setRegion(coordinateRegion, animated: true)
-            }
         }
     }
+    
+    
+    
+ 
+
+
 }
